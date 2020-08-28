26e496fc7ee7841ac7c41589f59a8312e95b2b32
Gosia Borzecka <gosia.borzecka@outlook.com>
2020-08-28 12:37:48 +0100

    package update

---
+++ build/precache-manifest.227694b1b0671d0bd65887bcb078ca41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b348eeaec34385c80ab71017eca15e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9597c9b7ec47ab16713f",
+    "url": "/static/css/main.8f8a5cc3.chunk.css"
+  },
+  {
+    "revision": "9c520034a47c3aa43b8b",
+    "url": "/static/js/2.c5b8b7a3.chunk.js"
+  },
+  {
+    "revision": "11cfc49acd1819f3e804b64601daa210",
+    "url": "/static/js/2.c5b8b7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9597c9b7ec47ab16713f",
+    "url": "/static/js/main.b15f6f75.chunk.js"
+  },
+  {
+    "revision": "476d8052697eb866abc2",
+    "url": "/static/js/runtime-main.6e8befb4.js"
+  }
+]);
